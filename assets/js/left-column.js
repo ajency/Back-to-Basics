882cb46294318bfa63f394d77bd12c84f365887b
Ajaj Rajguru <ajaj@ajency.in>
2016-02-24 12:58:54 +0530

    Generated min files for js and css and fixed minor bugs

--- assets/js/left-column.js
+++ assets/js/left-column.js
@@ -36,13 +36,19 @@ $grid.imagesLoaded().progress( function() {
         });
 
       // jQuery to collapse the navbar on scroll
-      // $(window).scroll(function() {
-      //     if ($(".navbar").offset().top > 50) {
-      //         $(".navbar-fixed-top").addClass("top-nav-collapse");
-      //     } else {
-      //         $(".navbar-fixed-top").removeClass("top-nav-collapse");
-      //     }
-      // });
+      if ($(window).width() > 961) {
+        $(window).scroll(function() {
+          if ($(".navbar").offset().top > 50) {
+              $(".navbar-fixed-top").addClass("top-nav-collapse");
+          } else {
+              $(".navbar-fixed-top").removeClass("top-nav-collapse");
+          }
+        });
+      }
+      else
+      {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+      }
       
       // jQuery for page scrolling feature - requires jQuery Easing plugin
       $(function() {
