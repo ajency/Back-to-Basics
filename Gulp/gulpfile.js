f95b84453b68b0b730cfb119cf2932fe0cbe151a
Ajaj Rajguru <ajaj@ajency.in>
2016-03-18 15:52:34 +0530

    Updated Gulp tasks and updated folder tree structure.

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -9,16 +9,16 @@
 // Minify Js files
     var uglify = require('gulp-uglify');
 
+//Concat files
+    var concat = require('gulp-concat');
 
 
-// Tasks
+// Tasks(Command - gulp)
 
 
 // Minify Js files
 
-// Command- gulp uglify-js
-
-// Custom variable for changing css source(Please change here to minify JS)
+// Custom variable for theme JS file path
     var js_source = ['../assets/js/typical.js','../assets/js/left-column.js'];
 
 // Output destionation file
@@ -32,10 +32,28 @@
     });
 
 
+//To combine and minify js files.
+
+
+// Custom variable for combining all JS assests and combining it into one.
+    var js_source1 = ['../assets/js/plugins/jquery.easing.min.js','../assets/js/plugins/masonry.pkgd.min.js','../assets/js/plugins/imagesloaded.pkgd.js','../assets/js/plugins/animsition.min.js','../assets/js/plugins/jquery.mousewheel.min.js','../assets/js/plugins/lightgallery.js','../assets/js/plugins/lg-fullscreen.js','../assets/js/plugins/lg-thumbnail.js','../assets/js/plugins/lg-video.js','../assets/js/plugins/lg-autoplay.js','../assets/js/plugins/lg-zoom.js','../assets/js/plugins/lg-pager.js','../assets/js/plugins/Responsive-Timeline.js'];
+
+// Output destionation file
+    var js_dest1 = '../assets/js/plugins';
+
+    gulp.task('combine-js', function(){
+            return gulp.src(js_source1)
+                .pipe(uglify())
+                .pipe(concat('plugins.js'))
+                .pipe(rename('plugins.min.js'))
+                .pipe(gulp.dest(js_dest1));
+    });
+
+
 
 // Default gulp task
 
-gulp.task('default', ['uglify-js'], function() {
+gulp.task('default', ['uglify-js','combine-js'] , function() {
 
 // to watch files add tasks here
 
