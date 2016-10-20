9d92590a0183b3685a6a8ba36a09dd4679f7108e
Renuka Ghate <renuka@ajency.in>
2016-10-20 11:33:17 +0530

    js is formatted

--- assets/js/corporate.js
+++ assets/js/corporate.js
@@ -10,48 +10,46 @@
 
 $(document).ready(function() {
 
-       //carousel options
-            $('#quote-carousel').carousel({
-            pause: true, interval: 10000,
-            });
+  //carousel options
+  $('#quote-carousel').carousel({
+    pause: true, interval: 10000,
+  });
 
-
-      //Loader Animation
-        $(".animsition").animsition({
-            inClass: 'fade-in',
-            outClass: 'fade-out',
-            inDuration: 1500,
-            outDuration: 800,
-            loading: true,
-            loadingParentElement: 'body', //animsition wrapper element
-            loadingClass: 'animsition-loading',
-            loadingInner: '', // e.g '<img src="loading.svg" />'
-            timeout: true,
-            timeoutCountdown: 5000,
-            onLoadEvent: true,
-            overlay:false,
-         
-        });
-        $('#quote-carousel').carousel({
-          pause: true, interval: 10000,
-        });
+  //Loader Animation
+  $(".animsition").animsition({
+    inClass: 'fade-in',
+    outClass: 'fade-out',
+    inDuration: 1500,
+    outDuration: 800,
+    loading: true,
+    loadingParentElement: 'body', //animsition wrapper element
+    loadingClass: 'animsition-loading',
+    loadingInner: '', // e.g '<img src="loading.svg" />'
+    timeout: true,
+    timeoutCountdown: 5000,
+    onLoadEvent: true,
+    overlay:false,
+  });
+  $('#quote-carousel').carousel({
+    pause: true, interval: 10000,
+  });
         
-         //Accordion Active
-         $(".panel.panel-default").removeClass("activePanel");
-         $('.panel-collapse').on('show.bs.collapse', function () {
-           // alert("fkjdsf");
-           $(".panel.panel-default").removeClass("activePanel");
-           $(this).parents(".panel.panel-default").addClass("activePanel");
-         }) 
-         $('.panel-collapse').on('hidden.bs.collapse', function () {
-           // alert("fkjdsf");        
-           $(this).parents(".panel.panel-default").removeClass("activePanel");
-         })  
-
-         $('#stickySocial').find('#stickyBtn').each(function(){
-  var $el = $(this);
-  var ssCount = $el.data("count");
-  var ssClass = $el.attr("class").split(' ')[0];
-  $('.'+ssClass+' .count').html(ssCount);
-});
+  //Accordion Active
+  $(".panel.panel-default").removeClass("activePanel");
+  $('.panel-collapse').on('show.bs.collapse', function () {
+    // alert("fkjdsf");
+    $(".panel.panel-default").removeClass("activePanel");
+    $(this).parents(".panel.panel-default").addClass("activePanel");
+  }) 
+  $('.panel-collapse').on('hidden.bs.collapse', function () {
+    // alert("fkjdsf");        
+    $(this).parents(".panel.panel-default").removeClass("activePanel");
+  })  
+  $('#stickySocial').find('#stickyBtn').each(function(){
+    var $el = $(this);
+    var ssCount = $el.data("count");
+    var ssClass = $el.attr("class").split(' ')[0];
+    $('.'+ssClass+' .count').html(ssCount);
+  });
+  
 });
